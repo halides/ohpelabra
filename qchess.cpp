520089ef85e68b915994527beb87d100d932941d
Pekka O. Korolainen <pekka.korolainen@gmail.com>
2016-04-20 22:48:13 +0300

    i'm gonna refactor, pushing a working version before that

--- qchess.cpp
+++ qchess.cpp
@@ -5,7 +5,6 @@
 #include <algorithm>
 
 int main () {
-    std::cout << "hello quantum world\n\n";
     Board b;
     Player& wp = b.get_player("white");
     Player& bp = b.get_player("black");
@@ -27,25 +26,21 @@ int main () {
                 std::cout << p.get_pos().file << (int)p.get_pos().rank << ": " << p.get_ech() << " q: " << p.get_qech() << " " << (p.get_state()?"ALIVE":"DEAD") << "\n";
             }
             std::cout << "\n";
-        }
-        else if (input.compare(0,2,"s ") == 0) {
-std::cout << "mentiin tänne\n";
+        } else if (input.compare(0,2,"s ") == 0) {
             Position pos {input.at(2), input.at(3)-48}; //yay magic!
             for (Piece& p: wp.get_pieces()) {
                 if(p.get_pos() == pos) {
-std::cout << "mentiin tonne\n";
                      echelon ech = p.get_random_ech();
-                     auto moves = b.get_moves(ech, p);
-                     for (Position pose: moves) {
-std::cout << "päästiin tänne\n";
-                         std::cout << "a pose: "<< pose.file << (int)pose.rank << std::endl;
+                     std::cout << "The piece will be: " << p.ech_to_str(ech) << "\nPossible moves: ";
+                     for (Position pose: b.get_moves(ech,p)) {
+                         std::cout << pose.file << (int)pose.rank << " ";
                      }
-                     std::cout << "The piece will be: " << p.ech_to_str(ech) << "\nWhere do you want to move it to: ";
+                     std::cout << "\nWhere do you want to move it to: ";
+                     getline(std::cin, input);
+                     wp.move(pos, Position {input.at(0), input.at(1)-48});
                      break;
                 }
             }
-            getline(std::cin, input);
-            wp.move(pos, Position {input.at(0), input.at(1)-48});
         }
 
         std::cout << "Please input command: ";
